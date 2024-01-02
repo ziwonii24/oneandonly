3b338d710b2148e817e3200f34b6eb157d336996
ziwonii24 <ziwonii25@gmail.com>
2024-01-02 20:14:02 +0900

    :tada: migrate to next app

--- src/components/CommingSoon.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const CommingSoon: React.FC = () => <h1>Comming Soon</h1>;
-
-export default CommingSoon;
--- src/components/RouterTest.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const RouterTest: React.FC = () => <h1>Test</h1>;
-
-export default RouterTest;
